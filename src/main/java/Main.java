9057bc1af9b001f773ff6e080cc8e2dc8f1e2e0d
roshanprasai <200598344@student.georgianc.on.ca>
2025-08-04 10:07:00 -0400

    converter controller updated

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,21 @@
-public class Main {
+package main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/converter.fxml"));
+        primaryStage.setTitle("Currency Converter");
+        primaryStage.setScene(new Scene(root, 400, 300));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
--- src/main/java/controller/ConverterController.java
+++ src/main/java/controller/ConverterController.java
@@ -1,4 +1,43 @@
-package controller;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import java.util.Map;
 
-public class ConverterController {
+public class Controller {
+    @FXML
+    private TextField amountField;
+    @FXML
+    private ComboBox<String> fromCurrency;
+    @FXML
+    private ComboBox<String> toCurrency;
+    @FXML
+    private Label resultLabel;
+
+    private Map<String, Double> rates;
+
+    @FXML
+    public void initialize() {
+        ExchangeRates data = APIService.fetchRates("USD");
+        if (data != null) {
+            rates = data.getConversionRates();
+            fromCurrency.getItems().addAll(rates.keySet());
+            toCurrency.getItems().addAll(rates.keySet());
+            fromCurrency.setValue("USD");
+            toCurrency.setValue("EUR");
+        } else {
+            resultLabel.setText("Failed to fetch data.");
+        }
+    }
+
+    @FXML
+    public void handleConvert() {
+        try {
+            double amount = Double.parseDouble(amountField.getText());
+            double from = rates.get(fromCurrency.getValue());
+            double to = rates.get(toCurrency.getValue());
+            double converted = (amount / from) * to;
+            resultLabel.setText(String.format("%.2f %s = %.2f %s", amount, fromCurrency.getValue(), converted, toCurrency.getValue()));
+        } catch (Exception e) {
+            resultLabel.setText("Invalid input or error.");
+        }
+    }
 }
